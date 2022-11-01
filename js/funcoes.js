4383ded5199c59e7685984582b592f256caf0d23
etim1 <etim1@gmail.com>
2022-11-01 11:10:38 -0300

    Atualizado

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,4 +1,4 @@
-//Criar uma variável para manipular o XML
+/*//Criar uma variável para manipular o XML
 xmlhttp = new XMLHttpRequest();
 //Seleciona o arquivo XML
 xmlhttp.open("GET","xml/conteudo.xml",false);
@@ -37,5 +37,48 @@ function postagem(){
                 "<td>"+ x[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue +"</td>" +
                 "<td>"+ x[i].getElementsByTagName("corpo")[0].childNodes[0].nodeValue + "</td>" +
             "</tr>");
+    
+}*/
+
+
+//Criar uma variável para manipular o XML
+xmlhttp = new XMLHttpRequest();
+//Seleciona o arquivo XML
+xmlhttp.open("GET","xml/conteudo.xml",false);
+//Enviar o arquivo para leitura
+xmlhttp.send();
+//Informar que o arquivo enviado é um XML
+xmlDoc = xmlhttp.responseXML;
+
+//Armazena as postagens do XML em um array
+x = xmlDoc.getElementsByTagName("postagem");
+
+function teste(){
+    for(i=x.length-1;i>=0;i--){
+        document.write(
+            "<div>" +
+                "<div>"+ x[i].getAttribute("codigo") +"</td>" +
+                "<div><img src='imgs/"+ x[i].getElementsByTagName("imagem")[0].childNodes[0].nodeValue +"'width='150'></div>" +
+                "<div><a href='postagem.html?codigo_postagem=" + i + "'>" + x[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue +"</a></div>" +
+                "<div>"+ x[i].getElementsByTagName("corpo")[0].childNodes[0].nodeValue.substr(0,150) +"...</div>" +
+            "</div>");
+    }
+}
+
+function postagem(){
+    //Armazena a URL completa, ex: http://localhost:81/blog/postagem.html?codigo_postagem=2
+    url = new URL(window.location.href);
+    //Armazena o parâmetro, ex: codigo_postagem=2
+    parametro = url.searchParams;
+    //Armazena a id, ex: 2
+    i = parametro.get("codigo_postagem");
+
+        document.write(
+            "<div>" +
+                "<div>"+ x[i].getAttribute("codigo") +"</div>" +
+                "<div><img src='imgs/"+ x[i].getElementsByTagName("imagem")[0].childNodes[0].nodeValue +"'width='150'></div>" +
+                "<div>"+ x[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue +"</div>" +
+                "<div>"+ x[i].getElementsByTagName("corpo")[0].childNodes[0].nodeValue + "</div>" +
+            "</div>");
     
 }
