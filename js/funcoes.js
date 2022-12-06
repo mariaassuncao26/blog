3c7107b4eb6777df5ef80060c0fd6adcd80d7f16
etim1 <etim1@gmail.com>
2022-12-06 14:36:58 -0300

    Atualizado

--- js/funcoes.js
+++ js/funcoes.js
@@ -40,6 +40,51 @@ function postagem(){
     
 }*/
 
+/*
+//Criar uma variável para manipular o XML
+xmlhttp = new XMLHttpRequest();
+//Seleciona o arquivo XML
+xmlhttp.open("GET","xml/conteudo.xml",false);
+//Enviar o arquivo para leitura
+xmlhttp.send();
+//Informar que o arquivo enviado é um XML
+xmlDoc = xmlhttp.responseXML;
+
+//Armazena as postagens do XML em um array
+x = xmlDoc.getElementsByTagName("postagem");
+
+function teste(){
+    for(i=x.length-1;i>=0;i--){
+        document.write(
+            "<div class='card' style='width:400px'>" +
+                "<div>" + 
+                    "<img class='card-img-top' src='imgs/"+ x[i].getElementsByTagName("imagem")[0].childNodes[0].nodeValue +"'width='150'>" + 
+                "</div>" + 
+                "<div class='card-body'>" +
+                    "<h4 class='card-title'><a href='postagem.html?codigo_postagem=" + i + "'>" + x[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue +"</a></h4>" +
+                    "<p class='card-text'>"+ x[i].getElementsByTagName("corpo")[0].childNodes[0].nodeValue.substr(0,28) +"...</p>" +
+                 "</div>" +
+            "</div>");
+    }
+}
+
+function postagem(){
+    //Armazena a URL completa, ex: http://localhost:81/blog/postagem.html?codigo_postagem=2
+    url = new URL(window.location.href);
+    //Armazena o parâmetro, ex: codigo_postagem=2
+    parametro = url.searchParams;
+    //Armazena a id, ex: 2
+    i = parametro.get("codigo_postagem");
+
+        document.write(
+            "<div>" +
+                "<div><img src='imgs/"+ x[i].getElementsByTagName("imagem")[0].childNodes[0].nodeValue +"'width='150'></div>" +
+                "<div>"+ x[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue +"</div>" +
+                "<div>"+ x[i].getElementsByTagName("corpo")[0].childNodes[0].nodeValue + "</div>" +
+            "</div>");
+    
+}*/
+
 
 //Criar uma variável para manipular o XML
 xmlhttp = new XMLHttpRequest();
@@ -56,10 +101,15 @@ x = xmlDoc.getElementsByTagName("postagem");
 function teste(){
     for(i=x.length-1;i>=0;i--){
         document.write(
-            "<div class='c'>" +
-                "<div><img src='imgs/"+ x[i].getElementsByTagName("imagem")[0].childNodes[0].nodeValue +"'width='150'></div>" +
-                "<div><a href='postagem.html?codigo_postagem=" + i + "'>" + x[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue +"</a></div>" +
-                "<div>"+ x[i].getElementsByTagName("corpo")[0].childNodes[0].nodeValue.substr(0,28) +"...</div>" +
+            "<div class='card' style='width:400px'>" +
+                "<div>" + 
+                    "<img class='card-img-top' src='imgs/"+ x[i].getElementsByTagName("imagem")[0].childNodes[0].nodeValue +"'width='150'>" + 
+                "</div>" + 
+                "<div class='card-body'>" +
+                    "<h4 class='card-title'>" + x[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue + "</h4>" +
+                    "<p class='card-text'>"+ x[i].getElementsByTagName("corpo")[0].childNodes[0].nodeValue.substr(0,100) +"...</p>" +
+                    "<div class='btn'><a href='postagem.html?codigo_postagem=" + i + "'>" + x[i].getElementsByTagName("link")[0].childNodes[0].nodeValue +"</a></div>" +
+                 "</div>" +
             "</div>");
     }
 }
